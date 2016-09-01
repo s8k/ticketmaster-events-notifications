499196bd8e1ca16a30c68ebbc2900b2370a63dd8
Dzmitry Lapunou <d.lopunov@gmail.com>
2016-09-01 15:56:23 +0300

    Add bootstrap theme

--- app.js
+++ app.js
@@ -30,6 +30,8 @@ app.use('/events', events)
 app.use('/around', around)
 
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
