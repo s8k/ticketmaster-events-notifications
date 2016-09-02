cc7b31b754057992e9609a88c3e9cbf80883ce0b
Yauhen Karelau <Yauhen_Karelau@epam.com>
2016-09-02 20:07:37 +0100

    Test config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 var config = {
     spotify: {
         clientId: '24de3f695f4b427ab719fb9cf84f1518',
-        cliendSecret: '<not in git>',
+        cliendSecret: '<hidden> ',
         callbackUrl: 'http://localhost:3000/signin/spotify/callback'
     },
     lastfm: {
         clientId: 'c8194525e29b50a2db508253ec72e6e1',
-        cliendSecret: '<not in git>',
+        cliendSecret: '<hidden>',
         callbackUrl: 'http://localhost:3000/signin/lastfm/callback'
     }
 };
