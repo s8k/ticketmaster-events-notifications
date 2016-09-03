87f624b2369a53cd9551778efb293ba7ea5dbdd8
Dzmitry Lapunou <d.lopunov@gmail.com>
2016-09-03 16:21:39 +0100

    Last min update

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ var config = {};
 config.spotify = {
     clientId: '24de3f695f4b427ab719fb9cf84f1518',
     clientSecret: '1750923bb05a422e96636619add5c997',
-    callbackUrl: 'http://localhost:3000/signin/spotify/callback'
+    callbackUrl: 'http://tickets-notify.azurewebsites.net/signin/spotify/callback'
 };
 
 config.tm = {
     clientId: 'IJ5f9Esalq48QebA7t8ENydSGQAGhVIu',
     clientSecret: 'oTMGdDSnnekswaUM',
-    callbackUrl: 'http://localhost:3000/signin/lastfm/callback'
+    callbackUrl: 'http://tickets-notify.azurewebsites.net/signin/lastfm/callback'
 };
 
 config.db = {
--- jobs.js
+++ jobs.js
@@ -3,32 +3,32 @@ var usersDb = require('./db/users');
 var ticketmaster = require('./ticketmaster');
 
 // Main notifications sender job
-var j = schedule.scheduleJob('*/1 * * * *', function () {
+var j = schedule.scheduleJob('*/1 * * * *', function() {
     usersDb
         .readAllProfiles()
         .toArrayAsync()
-        .then(function (result) {
+        .then(function(result) {
             var users = result.feed;
-            users.forEach(function (u) {
-//                u.topArtists.forEach(function (a) {
-                    // ticketmaster
-                    //     .getEventsByArtistAndLocation(u.location.latitude, u.location.longitude, u.location.radius, a.name)
-                    //     .then(function (result) {
-                            u.notifications = u.notifications ? u.notifications : [];
-                            u.notifications.push('Mando Diao has a concert on October, 30 in London.');
-                            usersDb
-                                .upsertProfile(u)
-                                .fail(function (error) {
-                                    console.log('Error occured while pushing notification');
-                                });
-                    //     })
-                    //     .fail(function (error) {
-                    //         console.log(`Error occured while reading user profile: ${u.id}`);
-                    //     });
-  //              }, this);
+            users.forEach(function(u) {
+                //                u.topArtists.forEach(function (a) {
+                // ticketmaster
+                //     .getEventsByArtistAndLocation(u.location.latitude, u.location.longitude, u.location.radius, a.name)
+                //     .then(function (result) {
+                u.notifications = u.notifications ? u.notifications : [];
+                u.notifications.push("Shura has a concert on December, 7 in London. <a href='http://www.ticketmaster.co.uk/shura-london-07-12-2016/event/1F0050DD98F540CA?artistid=2039422&majorcatid=10001&minorcatid=1'>See the event</a>");
+                usersDb
+                    .upsertProfile(u)
+                    .fail(function(error) {
+                        console.log('Error occured while pushing notification');
+                    });
+                //     })
+                //     .fail(function (error) {
+                //         console.log(`Error occured while reading user profile: ${u.id}`);
+                //     });
+                //              }, this);
             }, this);
         })
-        .fail(function (error) {
+        .fail(function(error) {
             console.log('Error occured while reading all profiles');
         });
 
